b2df7c673e47220f8e7398cd3822c86b616b12ed
alexandre philibeaux <alex.philibeaux@gmail.com>
2017-02-12 13:43:35 +0100

    test

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ app.get('/colissimo',function(req,res){
         result = data.children().text().trim();
         head = data.children().last().children().last().text().trim();
 
+
         json.result = result;
         json.head = head;
       })
@@ -69,11 +70,45 @@ app.get('/colissimo',function(req,res){
       console.log('File successfully written! - Check your project directory for the output.json file');
     })
     */
-    res.send(json)
+
+   res.send(json)
     //res.send('hey look your output')
   })
 })
 
-app.listen('8081')
-console.log('Magic happens on port 8081');
+app.get('/ups',function(req,res){
+  url = 'https://wwwapps.ups.com/WebTracking/track';
+
+  request(url,function(error,response,html){
+    if(!error){
+      var $ = cheerio.load(html);
+
+      var result,head;
+      var json ={test :"",result:"", head:""};
+
+      $('#collapse3').filter(function(){
+        var data = $(this);
+        console.log(data);
+        test = data;
+        result = data.children().text().trim();
+        head = data.children().last().children().last().text().trim();
+
+        json.test=test;
+        json.result = result;
+        json.head = head;
+      })
+    }
+/*
+    fs.writeFile('colissimo.json', JSON.stringify(json, null, 4), function(err){
+      console.log('File successfully written! - Check your project directory for the output.json file');
+    })
+    */
+
+   res.send(json)
+    //res.send('hey look your output')
+  })
+})
+
+//app.listen('8081')
+//console.log('Magic happens on port 8081');
 exports = module.exports = app;
