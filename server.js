1a3ffc133198603eeb254ff05669bb27e0e81576
alexandre philibeaux <alex.philibeaux@gmail.com>
2017-02-14 11:43:21 +0100

    Test Heroku

--- server.js
+++ server.js
@@ -109,6 +109,6 @@ app.get('/ups',function(req,res){
   })
 })
 
-//app.listen('8081')
-//console.log('Magic happens on port 8081');
+app.listen('8081')
+console.log('Magic happens on port 8081');
 exports = module.exports = app;
