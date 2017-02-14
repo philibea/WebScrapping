4fbcfc731256fc9a945810478ce7cabd8e8454f7
alexandre philibeaux <alex.philibeaux@gmail.com>
2017-02-14 12:21:30 +0100

    premier essai

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+  // ejs render automatically looks in the views folder
+     res.render('index');
+})
 
 app.get('/scrape', function(req, res){
   // Let's scrape Anchorman 2
